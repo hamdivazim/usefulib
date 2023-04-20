a610549578d8dccc2bdbf0911e57903ca71beeb6
hamdivazim <coding.wizard4@gmail.com>
2023-04-20 21:28:38 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 DESCRIPTION = 'A collection of assorted methods to make small tasks easier..'
 LONG_DESCRIPTION = """
 # usefulib v1.0
