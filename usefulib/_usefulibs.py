75c076ca67ed6adca9d7785fe08bde09cf35013d
ShadowStrike-Atomiser <132094151+ShadowStrike-Atomiser@users.noreply.github.com>
2023-04-30 18:10:20 +0100

    Added convert_base()

--- usefulib/_usefulibs.py
+++ usefulib/_usefulibs.py
@@ -160,3 +160,27 @@ def denary_to_ternary(n):
     res= ''.join(reversed(nums))
 
     return int(res)
+def convert_base():
+    """@ShadowStrike-Atomiser - Converts a number from one base to another"""
+    from_base = int(input("Type a base:"))
+    to_base = int(input("Type another base (2-16): "))
+    num = input("Type a number in base " + str(from_base) + ": ")
+
+    base_10_num = 0
+    power = 0
+    for digit in num[::-1]:
+        if digit.isdigit():
+            base_10_num += int(digit) * (from_base ** power)
+        else:
+            base_10_num += (ord(digit.upper()) - 55) * (from_base ** power)
+        power += 1
+
+    new_num = ""
+    while base_10_num > 0:
+        digit = base_10_num % to_base
+        if digit < 10:
+            new_num = str(digit) + new_num
+        else:
+            new_num = chr(ord('A') + digit - 10) + new_num
+        base_10_num //= to_base
+    print("The number in base " + str(to_base) + " is:", new_num)
